4dd92a8cb20cc933ce8f2b3d1974ac249e2c6ab9
vlaadg <75899047+vlaadg@users.noreply.github.com>
2021-12-10 00:44:28 +0300

    feat: add order model

---
+++ src/resources/order/order.model.js
@@ -0,0 +1,32 @@
+const uuid = require('uuid');
+
+class Order {
+    constructor({
+        id = uuid(),
+        consumerId = 'null',
+        date = '24.05.2001',
+        deliveryTime = '1 hour'
+    } = {}) {
+        this.id = id;
+        this.consumerId = consumerId;
+        this.date = date;
+        this.deliveryTime = deliveryTime;
+    }
+
+    static toResponse(order) {
+        const {
+            id,
+            consumerId,
+            date,
+            deliveryTime
+        } = order;
+        return {
+            id,
+            consumerId,
+            date,
+            deliveryTime
+        };
+    }
+}
+
+module.exports = Order;
