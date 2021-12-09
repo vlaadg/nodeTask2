9c911ecabcd553325a02c69711d25b40738bd519
vlaadg <75899047+vlaadg@users.noreply.github.com>
2021-12-10 01:00:59 +0300

    feat add product memort repository

---
+++ src/resources/product/product.memory.repository.js
@@ -0,0 +1,68 @@
+const Product = require('./product.model')
+
+const products = [new Product()]
+
+const getAll = async () => products;
+
+const getById = async (id) => products.find((product) => product.id == id);
+
+const createProduct = async ({
+    orderId,
+    title,
+    description,
+    price
+}) => {
+    const product = new Product({
+        orderId,
+        title,
+        description,
+        price
+    })
+    products.push(product)
+    return product
+}
+
+const deleteById = async (id) => {
+    const productPos = products.findIndex((product) => product.id == id);
+
+    if (productPos === -1) return null;
+
+    const productDeletable = products[productPos];
+
+    products.splice(productPos, 1);
+    return productDeletable;
+}
+
+const updateById = async ({
+    orderId,
+    title,
+    description,
+    price
+}) => {
+    const productPos = products.findIndex((product) => proudct.id == id);
+
+    if (productPos === -1) return null;
+
+    const oldProduct = products[productPos]
+
+    const newProduct = {
+        ...oldProduct,
+        orderId,
+        title,
+        description,
+        price
+    };
+
+    products.splice(productPos, 1, newProduct);
+    return newProduct;
+
+};
+
+module.exports = {
+    products,
+    getAll,
+    getById,
+    createProduct,
+    deleteById,
+    updateById
+}
